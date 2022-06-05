8cc73af28afc45359975481c1c5d48a771b8dd60
naresh277 <56402169+naresh277@users.noreply.github.com>
2022-06-05 14:40:22 +0545

    Update settings.py

--- stockprediction/settings.py
+++ stockprediction/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT =   BASE_DIR, 'staticfiles'
+STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
